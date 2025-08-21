d32bb1680802ba65945e34c9f6a5527392f1814c
hsuncho <hsuncho@naver.com>
2025-08-21 11:33:17 +0900

    #2 sql: 함수 실습(문자열처리, 숫자/집계)

--- lgcns-src.sql
+++ lgcns-src.sql
@@ -1,8 +1,19 @@
+-- Day01
+
 SHOW DATABASES;
 USE inspire;
 SHOW TABLES;
 
 /*
+SQL(Structure Query Language)
+
+Select Query
+DDL : create, drop, alter
+DML : insert, update, delete
+DCL : commit, rollback
+
+select : 데이터를 검색할 때 사용하는 문법
+
 select  column_name | * | 표현식 또는 함수를 포함하는 식 | [as] 별칭 | distinct column_name
 from    table_name
 [WHERE]     -- 행의 제한
@@ -74,5 +85,186 @@ SELECT * FROM employee
 WHERE DEPT_ID IN (60, 90);
 
 SELECT * FROM employee;
+
+-- Day02 함수 : select, where
+/*
+- 프로그램에서 반복적으로 사용되는 부분을 분리해서 서브 프로그램
+
+- 유형
+단일행 함수 : 문자열, 날짜, 숫자, 기타변형 함수
+복수형(그룹) 함수 : min, max, sum, count, avg
+
+
+*/
+
+SELECT *
+FROM employee;
+
+SELECT 	EMP_NAME, 
+			CONCAT(EMP_NAME, '님'),
+			LENGTH(EMP_NAME),
+			CHAR_LENGTH(EMP_NAME)
+FROM employee;
+
+SELECT CHAR(65);
+
+-- LOWER(), UPPER()
+
+SELECT LOWER('HELLO'), UPPER('lgcns');
+
+-- LPAD, RPAD : 자리수를 고정하여 빈 공간을 원하는 문자로 채우는 함수
+SELECT EMAIL AS `원본 테이터`
+		, LENGTH(EMAIL) `원본길이`
+		, LPAD(EMAIL, 30)
+		, RPAD(EMAIL, 30, '*')
+		, LENGTH(LPAD(EMAIL, 30))
+FROM employee;
+
+-- ELT() : 인덱스를 이용해서 특정 위치의 문자를 찾는 함수
+-- INSTR() : 문자열을 이용해서 부분 문자열을 찾는 함수
+
+SELECT ELT(2, '1', '2', '3'), INSTR('hsuncho', 's');
+
+SELECT EMAIL
+FROM employee;
+
+-- .앞의 문자 'c'의 인덱스 번지를 검색
+SELECT INSTR(EMAIL, '.') -1 AS idx
+FROM employee;
+
+-- LEFT(), RIGHT()
+SELECT LEFT('ABCDE', 3), RIGHT('ABCDE', 3);
+
+-- SUBSTRING()
+-- 부분문자열을 반환하는 함수
+SELECT SUBSTRING('ABCDE', 1, 2);
+
+SELECT SUBSTRING(EMAIL, INSTR(EMAIL, '.') - 1, 1)
+FROM employee;
+
+-- TRIM(), LTRIM(), RTRIM() : 제거(패턴을 제거하는 것은 아니다), 기본공백
+SELECT LTRIM('   LGCNS')
+		, RTRIM('LGCNS   ')
+		, TRIM('   LGCNS   ');
+
+SELECT TRIM(BOTH '123' FROM '123TECH123')
+		,TRIM(LEADING '123' FROM '123TECH123')
+		,TRIM(TRAILING '123' FROM '123TECH123');
+
+-- 문자열 반복 : REPEAT();
+SELECT REPEAT('LGCNS', 3);
+
+-- 문자열 치환 : REPLACE();
+SELECT REPLACE('오늘은 코스모스 졸업식 즐겨보자', '즐겨보자', '놀아보자' );
+
+-- SUBSTRING(문자열, 시작 위치, 길이) OR (문자열 FROM 시작위치 FOR 길이)
+-- SUBSTRING_INDEX(문자열, 구분자, 횟수)
+
+SELECT SUBSTRING('THIS IS INSPIRE CAMP', 9, 7)
+		, SUBSTRING('THIS IS INSPIRE CAMP' FROM 9 FOR 7);
+
+SELECT SUBSTRING_INDEX('www.LGCNS.com', '.', -1);
+
+/*
+employee
+1. 사원 이메일 중 아이디만 추출
+2. 입사년도만 추출
+3. 주민번호 앞 6자리만 추출
+4. 입사일 출력 포맷을 XXXX년 XX월 XX일
+*/
+
+SELECT * FROM employee;
+
+-- 1
+SELECT EMAIL, SUBSTRING_INDEX(EMAIL,'@',1)
+FROM employee;
+
+SELECT EMAIL, SUBSTRING(EMAIL, 1, INSTR(EMAIL, '@')-1)
+FROM employee;
+
+-- 2
+SELECT HIRE_DATE,
+		SUBSTRING(HIRE_DATE, 1, 4),
+		SUBSTRING_INDEX(HIRE_DATE,'-', 1)
+FROM employee;
+		
+-- 3
+SELECT EMP_NO,
+		SUBSTRING(EMP_NO, 1, 6),
+		SUBSTRING_INDEX(EMP_NO, '-', 1)
+FROM employee;
+
+-- 4
+SELECT HIRE_DATE,
+       CONCAT(
+           SUBSTRING(HIRE_DATE, 1, 4), '년 ',
+           SUBSTRING(HIRE_DATE, 6, 2), '월 ',
+           SUBSTRING(HIRE_DATE, 9, 2), '일'
+       )
+FROM employee;
+
+SELECT HIRE_DATE,
+		CONCAT(
+			SUBSTRING_INDEX(HIRE_DATE, '-', 1), '년 ',
+			SUBSTRING_INDEX(
+				SUBSTRING_INDEX(HIRE_DATE, '-', 2)
+			, '-', -1), '월 ',
+			SUBSTRING_INDEX(HIRE_DATE, '-', -1), '일'
+		)
+FROM employee;
+
+SELECT HIRE_DATE, 
+		DATE_FORMAT(HIRE_DATE, '%Y년 %m월 %d일')
+FROM employee;
+
+-- -------------------------------------
+
+SELECT *
+FROM usertbl;
+
+SELECT *
+FROM buytbl;
+
+-- 평균 구매 개수를 확인하고 싶다면?
+SELECT AVG(AMOUNT)
+FROM buytbl;
+
+SELECT CAST(AVG(AMOUNT) AS INT) AS `평균구매개수`
+FROM buytbl;
+
+-- ERROR
+-- SELECT INT(AVG(AMOUNT)) AS `평균구매개수`
+-- FROM buytbl;
+
+SELECT '100' + '100';
+SELECT CAST('100' AS INT) + CAST('100' AS INT);
+
+SELECT CAST(AVG(AMOUNT) AS SIGNED INTEGER) AS `평균구매개수`
+FROM buytbl;
+
+-- 구매 번호, 총 금액 (PRICE * AMOUNT = ), 구매액 검색
+SELECT NUM `구매번호`,
+		CONCAT(
+				CAST(PRICE AS VARCHAR(10)),
+				' * ',
+				CAST(AMOUNT AS VARCHAR(10))
+		) AS `총금액`
+		, PRICE * AMOUNT `구매액`
+FROM buytbl;
+
+SELECT NUM `구매번호`,
+		CONCAT(
+				PRICE,
+				' * ',
+				AMOUNT
+		) AS `총금액`
+		, PRICE * AMOUNT `구매액`
+FROM buytbl;
+
+SELECT LEFT(EMP_NO, 6),
+		RIGHT(EMP_NO, 7)
+		LEFT(EMP_NO, 6) + RIGHT(EMP_NO, 7)
+FROM employee;
+
 
 
